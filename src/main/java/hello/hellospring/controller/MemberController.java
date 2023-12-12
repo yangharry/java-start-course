07fe4e14984a8943f92187eacbd116ec2aab5e3a
yangharry <myid7771@naver.com>
2023-12-12 14:32:05 +0900

    스프링 빈과 의존관계

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,5 +1,6 @@
 package hello.hellospring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import hello.hellospring.service.MemberService;
@@ -7,9 +8,15 @@ import hello.hellospring.service.MemberService;
 @Controller
 public class MemberController {
 
-    private final MemberService memberService;
+    private MemberService memberService;
 
-    public MemberController(MemberService memberService) {
+    @Autowired
+    public void setMemberService(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    // @Autowired
+    // public MemberController(MemberService memberService) {
+    // this.memberService = memberService;
+    // }
 }
